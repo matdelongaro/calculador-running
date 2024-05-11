667e8c7ec66b4d8c05cbc7c66f125a956f360c59
matias delongaro <matiasadelongaro@gmail.com>
2024-05-11 16:24:33 -0300

    estilando

--- js/main.js
+++ js/main.js
@@ -4,12 +4,11 @@ function calcularRitmo(){
     let ritmo = tiempo/distancia;
     let minutosPorKm = Math.floor(ritmo);
     let segundosExtra = Math.floor((ritmo - minutosPorKm) * 60 );
-    document.getElementById('resultado').innerHTML = `Tu ritmo es ${minutosPorKm} minutos y ${segundosExtra} segundos por km`
+    document.getElementById('resultado').innerHTML = `Tu ritmo es de ${minutosPorKm}:${segundosExtra} `
 }
 
 function reset(){
     document.getElementById('distance').value = ''
     document.getElementById('time').value = ''
-        
-    
+    document.getElementById('resultado').innerHTML = ``
 }
